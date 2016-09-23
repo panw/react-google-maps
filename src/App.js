1882beb55a40caf8091ee7575611e36c8229ef6e
Pan Wangperawong <panw@outlook.com>
2016-09-23 11:04:17 -0400

    update code to avaoid google undefined error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-const ARC_DE_TRIOMPHE_POSITION = {
-  lat: 48.873947,
-  lng: 2.295038
-};
 
 const EIFFEL_TOWER_POSITION = {
   lat: 48.858608,
   lng: 2.294471
 };
+
 class App extends Component {
   componentDidMount() {
-    this.map = new google.maps.Map(this.refs.map, {
+    this.map = window.google.maps.Map(this.refs.map, {
       center: EIFFEL_TOWER_POSITION,
       zoom: 16
     });
   }
+
   render() {
     const mapStyle = {
       width: 500,
