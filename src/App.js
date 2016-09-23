98debd888b014bba6fa8ab14df9ef37bdb33498c
Pan Wangperawong <panw@outlook.com>
2016-09-23 12:39:56 -0400

    update readme with project description

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const EIFFEL_TOWER_POSITION = {
-  lat: 48.858608,
-  lng: 2.294471
-};
+// const EIFFEL_TOWER_POSITION = {
+//   lat: 48.858608,
+//   lng: 2.294471
+// };
 
 class App extends Component {
   componentDidMount() {
     this.map = new window.google.maps.Map(this.refs.map, {
-      center: EIFFEL_TOWER_POSITION,
-      zoom: 16
+      center: {lat: 0, lng: -180},
+      zoom: 2
     });
+
+    var flightPlanCoordinates = [
+      {lat: 37.772, lng: -122.214},
+      {lat: 21.291, lng: -157.821},
+      {lat: -18.142, lng: 178.431},
+      {lat: -27.467, lng: 153.027}
+    ];
+
+    var flightPath = new window.google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+    flightPath.setMap(this.map);
   }
 
   render() {
